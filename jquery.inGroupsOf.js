dc8af4ac26abebe4065000e51434b55b11f90393
Andy-Holland <andyholland1991@aol.com>
2013-02-01 15:00:30 +0000

    Update jquery.inGroupsOf.js

--- jquery.inGroupsOf.js
+++ jquery.inGroupsOf.js
@@ -3,6 +3,6 @@
     if ( typeof i == 'undefined' ) i = 1;
     var len = $(this).length;
     for( var start = 0; start < len; start += i )
-      callback.call(this, $(this).splice(start, i));
+      callback.call($(this).splice(start, i));
   };
 })(window, jQuery);
