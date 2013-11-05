b8607c8315c9fb3ffeaff53446c9b54ea90c2ec0
Andy-Holland <andyholland1991@aol.com>
2013-11-05 13:45:40 +0000

    Update jquery.inGroupsOf.js
    Edited to return jQuery array of groups

--- jquery.inGroupsOf.js
+++ jquery.inGroupsOf.js
@@ -1,8 +1,15 @@
 (function(window, $) {
+  'use strict';
   $.fn.inGroupsOf = function(i, callback) {
     if ( typeof i == 'undefined' ) i = 1;
-    var len = $(this).length;
-    for( var start = 0; start < len; start += i )
-      callback.call(this, $(this).splice(start, i));
+    var len    = $(this).length;
+    var groups = $([]);
+    for( var start = 0; start < len; start += i ) {
+      var index = groups.push($(this).splice(start, i));
+      if ( $.isFunction(callback) ) {
+        callback.call(this, groups[index-1]);
+      }
+    }
+    return groups;
   };
 })(window, jQuery);
